3d08f3bca9c009096e9846c613523f317396feb5
tuminhdiep <tuminh2307@gmail.com>
2025-04-30 21:05:32 -0400

    create the tile for the game

--- src/WhacAMole.java
+++ src/WhacAMole.java
@@ -6,6 +6,11 @@ public class WhacAMole {
     int boardHeight = 650;
 
     JFrame frame = new JFrame("Mario: Whac A Mole");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    JButton[] boards = new JButton[9];
+
 
     public WhacAMole() {
         frame.setVisible(true);
@@ -14,6 +19,26 @@ public class WhacAMole {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Score: 0");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3,3));
+        //boardPanel.setBackground(Color.black);
+        frame.add(boardPanel);
+
+        for (int i = 0; i<9 ; i++){
+            JButton tile = new JButton();;
+            boards[i] = tile;
+            boardPanel.add(tile);
+        }
+
     }
     
 
